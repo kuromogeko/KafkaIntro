5629eab1d231ad74c422079484573d9dd7c207fa
kuromogeko <ToteHosen3>
2020-07-01 16:05:37 +0200

    Updated for better demo of behaviour

--- src/main/java/SomeClassProducer.java
+++ src/main/java/SomeClassProducer.java
@@ -14,14 +14,30 @@ public class SomeClassProducer {
         config.put("bootstrap.servers", "192.168.10.39:9092");
         config.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         config.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+//        config.put("request.timeout.ms", "3000");
+//        config.put("timeout.ms", "3000");
+        config.put("max.block.ms", "3000");
+
         String topic = "test";
+
         KafkaProducer<String, String> producer = new KafkaProducer<String, String>(config);
         ProducerRecord<String, String> record = new ProducerRecord<>(topic, "value ohne key");
         ProducerRecord<String, String> recordWithKey = new ProducerRecord<>(topic, "key-nnkm", "Inhalt");
 
-         Future<RecordMetadata> recordFuture = producer.send(record);
+        Future<RecordMetadata> recordFuture = producer.send(record);
+        Future<RecordMetadata> recordFuture2 = producer.send(record, (recordMetadata, e) -> {
+            if (e != null) {
+                //rollback magic
+                System.out.println("Totally doing rollback magic");
+            } else {
+                System.out.println("Message ended up on partition: " + recordMetadata.partition());
+            }
+        });
         Future<RecordMetadata> recordFutureWithKey = producer.send(recordWithKey);
+
+
         recordFuture.get();
+        recordFuture2.get();
         recordFutureWithKey.get();
     }
 }
